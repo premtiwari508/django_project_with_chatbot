5cc6fbe783bc84dc78fd3f0c720c7e80c2008d2d
unknown <premtiwari508@gmail.com>
2019-06-13 23:57:59 +0530

    Last mod

--- Kids/models.py
+++ Kids/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class ICC(models.Model):
+    team_name = models.CharField(max_length=200)
+    matches = models.IntegerField()
+    matches_won = models.IntegerField()
+    matches_lost = models.IntegerField()
+    net_r_r = models.CharField(max_length=200)
+    points = models.IntegerField()
+
+    def __str__(self):
+        return self.team_name
+
+    class Meta:
+        db_table = "ICC"
 # Create your models here.
+
--- Kids/views.py
+++ Kids/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from . models import ICC
 
 def index(request):
-    return render(request,'Kids/index.html')
+    context = ICC.objects.all()
+    context = {'stat_data':context}
+    return render(request,'Kids/index.html',context)
 # Create your views here.
